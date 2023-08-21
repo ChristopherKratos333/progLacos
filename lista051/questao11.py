a75998ae482ce12b173a5c7ba04dd39cef0c2278
Christopher Alves <christo03082007@gmail.com>
2023-08-21 11:43:40 -0300

    Programação com While.

---
+++ lista051/questao11.py
@@ -0,0 +1,4 @@
+'''
+Elaborar um programa que apresente o valor de uma potência de uma base qualquer (Variável b) elevada a um
+expoente qualquer (Variável e), ou seja, de b elevado a e. (Sem usar Math.pow();)
+'''
