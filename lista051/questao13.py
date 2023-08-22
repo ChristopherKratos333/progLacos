dd936ba051144a4ca583e7cb03b543499a56b613
Christopher Alves <christo03082007@gmail.com>
2023-08-22 11:42:47 -0300

    Programação com While.

--- lista051/questao13.py
+++ lista051/questao13.py
@@ -12,19 +12,19 @@ print("Tabuada de 4:")
 cont1 = 1
 while cont1 <= 10:
     mult1 = 4 * cont1
-    print(f"3 . {cont1} = {mult1}")
+    print(f"4 . {cont1} = {mult1}")
     cont1 = cont1 + 1
 
 print("Tabuada de 5:")
 cont2 = 1
 while cont2 <= 10:
     mult2 = 5 * cont2
-    print(f"3 . {cont2} = {mult2}")
+    print(f"5 . {cont2} = {mult2}")
     cont2 = cont2 + 1
 
 print("Tabuada de 6:")
 cont3 = 1
 while cont3 <= 10:
     mult3 = 6 * cont3
-    print(f"3 . {cont3} = {mult3}")
+    print(f"6 . {cont3} = {mult3}")
     cont3 = cont3 + 1
--- lista051/questao15.py
+++ lista051/questao15.py
@@ -3,3 +3,4 @@ Desenvolver um programa que apresente a série de Fibonacci até o décimo quint
 formada pela sequência 1,1,2,3,5,8,13,21,34, ... etc. Ela se caracteriza pela soma de um termo posterior com seu
 anterior subsequente.
 '''
+
